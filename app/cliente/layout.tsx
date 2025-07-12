869619ff9449de5ea8e9a1dcbb2d83583ba5caba
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-07-11 17:19:21 -0700

    feat(cliente): creacion y modificacion del layout de cliente

--- app/cliente/layout.tsx
+++ app/cliente/layout.tsx
@@ -1,7 +1,34 @@
+'use client';
 
+import React from 'react';
+import { Layout } from 'antd';
 
-export default function layout() {
+import { usePersistentCollapse } from '@/hooks/usePersistentCollapse';
+import SidebarMenu from '@/components/layout/layoutCliente/SidebarMenu';
+import AdminHeader from '@/components/layout/layoutCliente/ClienteHeader';
+import { menuItems } from '@/components/layout/layoutCliente/clienteMenuItems';
+
+const { Content } = Layout;
+
+const user = {
+  name: 'Carlos Pérez',
+  role: 'Cliente',
+};
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const { collapsed, toggle } = usePersistentCollapse();
+
+  if (collapsed === null) return null;
+
   return (
-    <div>layout</div>
-  )
+    <Layout style={{ minHeight: '100vh' }}>
+      <SidebarMenu collapsed={collapsed} menuItems={menuItems} user={user} />
+      <Layout>
+        <AdminHeader collapsed={collapsed} toggleCollapse={toggle} />
+        <Content style={{ margin: '16px', padding: 24, background: '#fff' }}>
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
