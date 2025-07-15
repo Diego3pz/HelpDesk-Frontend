1c8fb86f150ed6188dee273eac4548638a76bd6b
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-07-15 16:59:11 -0700

    chore: actualiza configuración de axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,9 +1,11 @@
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost", 
-  withCredentials: true,
+  baseURL: "http://localhost/api", 
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+  },
 });
 
 export default api;
+
